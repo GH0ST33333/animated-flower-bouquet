b05acfdebca775d3995f474751a4a6d3b0d143aa
KSHITIJ <135329525+GH0ST33333@users.noreply.github.com>
2024-07-29 18:44:27 +0530

    trial code 1 for audio playing

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var playButton = document.getElementById("play-button");
+    var backgroundMusic = document.getElementById("background-music");
+
+    playButton.addEventListener("click", function() {
+        backgroundMusic.play();
+        playButton.style.display = "none"; // Hide the button after clicking
+    });
+});
