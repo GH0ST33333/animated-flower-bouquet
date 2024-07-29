3128a8eeca1592a17280fded96d00cd31b1cd362
KSHITIJ <135329525+GH0ST33333@users.noreply.github.com>
2024-07-29 18:51:00 +0530

    reverted previous changes

--- main.js
+++ main.js
@@ -5,12 +5,4 @@ onload = () => {
     }, 1000);
   };
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var playButton = document.getElementById("play-button");
-    var backgroundMusic = document.getElementById("background-music");
-
-    playButton.addEventListener("click", function() {
-        backgroundMusic.play();
-        playButton.style.display = "none"; // Hide the button after clicking
-    });
-});
+ 
